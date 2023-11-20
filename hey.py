c1dc3681bd55a1824efbde4068101ae4654f7dfc
LukeShively <60714723+LukeShively@users.noreply.github.com>
2023-11-19 21:47:40 -0500

    Update hey.py
    table already exists message will still appear

--- hey.py
+++ hey.py
@@ -1,10 +1,21 @@
 import evadb
 import string
-
 import os
 import pandas
-cursor = evadb.connect().cursor()
+import io
+import sys
+import contextlib
+import warnings
+from contextlib import contextmanager
 
+def evadb_format(unformatted):
+    formatted = unformatted
+    formatted = formatted.replace("\\n", "\n")
+    formatted = formatted.replace("\"", "@QUOTE@")
+    formatted = formatted.replace(";", "@SEMICOLON@")
+    return formatted
+
+cursor = evadb.connect().cursor()
 
 # Download ChatGPT UDF if needed
 #import wget
@@ -16,40 +27,43 @@ open_ai_key = os.environ.get('OPENAI_KEY')
 
 pandas.set_option('display.max_colwidth', None)
 
-# Drop Hey table if needed
-cursor.query("DROP TABLE IF EXISTS Hey;").df()
-
-# Then create Hey table
+# Create Hey table
 hey_query = """
-    CREATE TABLE IF NOT EXISTS Hey (request TEXT(500), answer TEXT(3000));
+    CREATE TABLE IF NOT EXISTS Hey (request TEXT(3000), answer TEXT(3000));
 """
 cursor.query(hey_query).df()
 
+hey_request = """"""
+file_list = []
+
+sys.argv[0] = ""
+for s in sys.argv:
+    s = str(s)
+    hey_request += s + " "
+    if "." in s and s[len(s) - 1] != "." and s.isalpha():
+        s = s.strip(",.?!\'\"()-")
+        file_list.append(s)
+
+for file in file_list:
+    try:
+        f = open(file, "r")
+    except:
+        continue
+    file_text = f.read().strip()
+    file_text = evadb_format(file_text)
+    
+    hey_request += " The file " + file + " is: " + file_text
+    print(f.read())
+
 #########################################################################
-# Input your request, set to "clear" to reset the table's state
-# Once input, run the code and your answer will be the output below!
-# A repeated input will use EvaDB's Hey table to reduce retrieval time
 
-hey_request = """hey generate a function in javascript"""
-
-#########################################################################
-
-valid_request = False
-if (hey_request.lower()[0:4] == "hey "):
-  hey_request = hey_request[4:]
-  valid_request = True
-else:
-  print("no hey? :(")
-  if hey_request.lower() == "clear":
+if hey_request.lower().strip() == "clear":
+    
     clear_query = """
             DROP TABLE IF EXISTS Hey;
     """
     cursor.query(clear_query).df()
-    hey_query = """
-    CREATE TABLE IF NOT EXISTS Hey (request TEXT(500), answer TEXT(3000));
-    """
-    cursor.query(hey_query).df()
-
+    exit()
 
 hey_test = f"""
     SELECT request FROM Hey
@@ -57,33 +71,30 @@ hey_test = f"""
 """
 
 answer = ""
-if (valid_request):
-  requested_query = cursor.query(hey_test).df()
-  if requested_query.empty:
+
+requested_query = cursor.query(hey_test).df()
+if requested_query.empty:
     chatgpt_udf = f"""
         SELECT ChatGPT("{hey_request}");
     """
     query = cursor.query(chatgpt_udf).df()
     answer = query.iloc[:,0].to_string()[1:].strip()
-    answer = answer.replace("\\n", "\n")
-    answer = answer.replace("\"", "@QUOTE@")
-    answer = answer.replace(";", "@SEMICOLON@")
+    answer = evadb_format(answer)
 
     hey_insertion = f"""
         INSERT INTO Hey (request, answer) VALUES
         ("{hey_request}", "{answer}");
     """
     cursor.query(hey_insertion).df()
-  else:
+else:
     hey_retrieval = f"""
         SELECT answer FROM Hey
         WHERE request = "{hey_request}";
     """
-
     query = cursor.query(hey_retrieval).df()
     answer = query.iloc[:,0].to_string()[1:].strip().replace("\\n", "\n")
 
 answer = answer.replace("@QUOTE@", "\"")
 answer = answer.replace("@SEMICOLON@", ";")
 
-print(answer)
+print("\n" + answer)
